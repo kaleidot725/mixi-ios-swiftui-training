2fc0facab458bc30161078bf1ad91e1c2c6db1c4
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-06-10 14:44:32 +0900

    Create project

---
+++ GitHubClient/GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 桂川 祐介 on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
